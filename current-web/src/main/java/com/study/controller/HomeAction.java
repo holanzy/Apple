e63f4452d66c9e5d33767f3589f97a7b33799c5c
holanzy <906294188@qq.com>
2019-12-10 22:21:18 +0800

    init

---
+++ current-web/src/main/java/com/study/controller/HomeAction.java
@@ -0,0 +1,15 @@
+package com.study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeAction {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String home() {
+        return "login";
+    }
+}
